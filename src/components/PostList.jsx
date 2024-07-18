c6ed4a6e6bf58b16234523d6c55537c68320da80
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-07-18 22:03:03 +0800

    added useState and Modal component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostList.module.css";
+import Modal from "./Modal";
 
 export default function PostList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
-    <ul className={styles.posts}>
-      <Post author="Ain Mutaqorrobin" body="terlalu kacak" />
-      <Post author="Mus'ab musaimmar" body="terlalu simple" />
-    </ul>
+    <>
+      <Modal>
+        <NewPost
+          onAuthorChange={authorChangeHandler}
+          onBodyChange={bodyChangeHandler}
+        />
+      </Modal>
+      <ul className={styles.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="ain" body="terlalu simple" />
+      </ul>
+    </>
   );
 }
